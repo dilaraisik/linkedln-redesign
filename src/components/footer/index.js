866b1d6faa1250aa99ad943ec3639919e4143aa8
dilaraisik <yagmurdilaraisik@gmail.com>
2022-08-07 16:04:00 +0300

    chore: update footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../assets/logo/Footer-Logo.png';
+import linkedinText from '../../assets/logo/LinkedIn.png';
 import QuestionIcon from '../../assets/icons/circle-question-regular.svg';
 
 
@@ -8,8 +9,9 @@ function Footer() {
     return (
         <div className=" w-full bg-footerBg border-t-footerLine border-t-solid border-t-2 container-md p-7 mt-10">
             <div className="grid grid-cols-4 gap-4">
-                <div>
+                <div className="flex-row">
                     <img className="ml-32 mt-7" src={Logo}/>
+                    <img className="ml-32 w-20 mt-2" src={linkedinText}/>
                 </div>
                 <div>
                     <p className="mb-8 text-textBlack text-sm font-bold">Navigation</p>
@@ -28,7 +30,7 @@ function Footer() {
                                 <a href="#!" className="text-gray-600">Small Business</a>
                             </p>
                         </div>
-                        <div className="">
+                        <div className="w-60 ml-10">
                             <p className="mb-4 text-textBlack text-sm">
                                 <a href="#!" className="text-gray-600">Talent Solutions</a>
                             </p>
@@ -42,7 +44,7 @@ function Footer() {
                                 <a href="#!" className="text-gray-600">Safery Center</a>
                             </p>
                         </div>
-                        <div className="">
+                        <div className="w-60 ml-10">
                             <p className="mb-4 text-textBlack text-sm">
                                 <a href="#!" className="text-gray-600">Community Guidelines</a>
                             </p>
@@ -56,8 +58,8 @@ function Footer() {
                     </div>
                 </div>
                 <div>
-                    <p className="mb-8 text-textBlack text-sm font-bold">Fast access</p>
-                    <div>
+                    <p className="mb-8 text-textBlack text-sm font-bold ml-20">Fast access</p>
+                    <div className="ml-20">
                         <button className="bg-blue text-white uppercase rounded p-4 text-xs inline-flex items-center">
                             <span>Questions?</span>
                             <img width="13" height="13"
@@ -66,7 +68,7 @@ function Footer() {
 
                         </button>
                     </div>
-                    <div className="mt-2.5">
+                    <div className="mt-2.5 ml-20">
                         <button className="bg-white text-blue uppercase border-blue border-2 border-solid rounded p-4 text-xs inline-flex items-center">
                             <span>Settings</span>
                             <img width="13" height="13"
@@ -74,9 +76,6 @@ function Footer() {
                                  src={QuestionIcon}/>
                         </button>
                     </div>
-                </div>
-                <div>
-                    <p className="mb-8 text-textBlack text-sm font-bold">Language</p>
                 </div>
             </div>
         </div>
